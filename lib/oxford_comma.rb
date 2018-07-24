9a2082f938597392b7698218ad02014c84943b4c
Forrest Benton <elliottbenton@gmail.com>
2018-07-24 00:00:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
     return array.to_s
-  elsif
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+  elsif array.length == 2 
+    return array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
